75a92acbfbc230ce8924ddeba36f2eb8b86f8a11
Sebastjan Senk <seba.senk@gmail.com>
2021-08-31 15:21:23 +0200

    popravki

--- model.py
+++ model.py
@@ -16,6 +16,6 @@ class Bojisce:
         self.sirina = visina
         self.visina = sirina
         self.stLadij = 0
-        self.polja = [ [Polje.Morje] * selgf.sirina for _ in rang(self.visina) ]
+        self.polja = [ [Polje.Morje] * self.sirina for _ in range(self.visina) ]
     
     def postaviLadjo(self, x, y, n, smer):
