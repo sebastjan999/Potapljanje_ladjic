14965827e105c02d6497743fa36a0b8df8b93f9c
Sebastjan Senk <seba.senk@gmail.com>
2021-09-01 17:25:35 +0200

    še opcija za pridruzitev v igro

--- static/lobby.js
+++ static/lobby.js
@@ -13,3 +13,27 @@
     location.replace('/igra/cakaj');
 }
 
+/**
+ * Pridruzi se igri ce obstaja
+ */
+ async function pridruzi() {
+    const id_igre = prompt("Vnesi id igre");
+
+    let obstaja = await $.ajax({
+        url: `/igra/obstaja/${id_igre}`,
+        type: "POST",
+        onerror: {}
+    });
+    if(obstaja != "true") {
+        alert("Igra ne obstaja");
+        return;
+    }
+    localStorage.setItem('id_igre', id_igre);
+    localStorage.setItem('igralec', 1);
+    await $.ajax({
+        url: `/igra/pridruzi/${id_igre}`,
+        type: "POST",
+        onerror: {}
+    });
+    location.replace('/igra/zgradi');
+}
