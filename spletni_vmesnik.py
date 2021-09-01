cd9817d70b44b3e5feb54f98d02ce9f8653e1868
Sebastjan Senk <seba.senk@gmail.com>
2021-09-01 18:02:01 +0200

    dodajanje v spletni vmesanik

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -30,3 +30,31 @@ def zmaga():
 @route('/igra/zguba')
 def zguba():
     return server_static('zguba.html')
+
+@post('/igra/ustvari')
+def ustvari():
+    return str(igra.ustvariVojno(2, 15, 15, 7))
+
+@post('/igra/pridruzi/<id_igre>')
+def pridruzi(id_igre):
+    if igra.igraObstaja(id_igre):
+        igra.pridruziIgralca(id_igre)
+        return 'true'
+    return 'Igra ne obstaja!'
+
+@post('/igra/obstaja/<id_igre>')
+def obstaja(id_igre):
+    if igra.igraObstaja(id_igre):
+        return 'true'
+    return 'false'
+
+@post('/igra/vsiIgralci/<id_igre>')
+def vsiIgralci(id_igre):
+    if igra.igraObstaja(id_igre) and igra.vrniVojno(id_igre).vsiIgralci():
+        return 'true'
+    return 'false'
+
+#dokoncaj se danes nujnoooo!!!!!!!!!(drgac bo cajt sou pol bos pa spet joku XD)
+#se za postavt ladjice, pa kko cilat, pa kdo zmagfa/zgubi pa te force treba nardit!!!
+
+##run(host='127.0.0.1', port=5002)
