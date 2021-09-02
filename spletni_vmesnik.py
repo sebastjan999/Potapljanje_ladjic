a1bd85c50780953d17c198bb450d98b32dcf0934
Sebastjan Senk <seba.senk@gmail.com>
2021-09-02 15:04:05 +0200

    shift + alt + F ;)

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -4,37 +4,46 @@ import json
 
 igra = tekstovni_vmesnik.Igra()
 
+
 @route('/static/<filename>')
 def server_static(filename):
     return static_file(filename, root='./static')
+
 
 @route('/')
 def lobby():
     return server_static('lobby.html')
 
+
 @route('/igra/cakaj')
 def cakaj():
     return server_static('cakaj.html')
+
 
 @route('/igra/zgradi')
 def zgradi():
     return server_static('zgradi.html')
 
+
 @route('/igra/vojna')
 def vojna():
     return server_static('vojna.html')
 
+
 @route('/igra/zmaga')
 def zmaga():
     return server_static('zmaga.html')
-    
+
+
 @route('/igra/zguba')
 def zguba():
     return server_static('zguba.html')
 
+
 @post('/igra/ustvari')
 def ustvari():
     return str(igra.ustvariVojno(2, 15, 15, 7))
+
 
 @post('/igra/pridruzi/<id_igre>')
 def pridruzi(id_igre):
@@ -43,11 +52,13 @@ def pridruzi(id_igre):
         return 'true'
     return 'Igra ne obstaja!'
 
+
 @post('/igra/obstaja/<id_igre>')
 def obstaja(id_igre):
     if igra.igraObstaja(id_igre):
         return 'true'
     return 'false'
+
 
 @post('/igra/vsiIgralci/<id_igre>')
 def vsiIgralci(id_igre):
@@ -60,22 +71,24 @@ def vsiIgralci(id_igre):
 def postaviLadjo(id_igre):
     if igra.igraObstaja(id_igre):
         igralec = int(request.forms['igralec'])
-        x       = int(request.forms['x'])
-        y       = int(request.forms['y'])
-        n       = int(request.forms['n'])
-        s       = request.forms['s']
+        x = int(request.forms['x'])
+        y = int(request.forms['y'])
+        n = int(request.forms['n'])
+        s = request.forms['s']
         igra.vrniVojno(id_igre).postaviLadjo(igralec, x, y, n, s)
         return 'true'
     return 'Igra ne obstaja!'
+
 
 @post('/igra/ustreli/<id_igre>')
 def ustreli(id_igre):
     if igra.igraObstaja(id_igre):
         igralec = int(request.forms['igralec'])
-        x       = int(request.forms['x'])
-        y       = int(request.forms['y'])
+        x = int(request.forms['x'])
+        y = int(request.forms['y'])
         return str(igra.vrniVojno(id_igre).ustreli(igralec, x, y))
     return 'Igra ne obstaja!'
+
 
 @post('/igra/vseLadje/<id_igre>')
 def vseLadje(id_igre):
@@ -92,27 +105,29 @@ def bojisca(id_igre, igralec):
     """
     if igra.igraObstaja(id_igre):
         out = {
-            "domace" : None,
-            "tuje" : []
+            "domace": None,
+            "tuje": []
         }
         v = igra.vrniVojno(id_igre)
         for i in range(v.stIgralcev):
             if i != igralec:
-                out["tuje"].append( v.kotNasprotnik(i) )
+                out["tuje"].append(v.kotNasprotnik(i))
             else:
                 out["domace"] = v.kotIgralec(i)
 
         return json.dumps(out)
     return 'Igra ne obstaja!'
 
+
 @post('/igra/naVrsti/<id_igre>/<igralec:int>')
 def naVrsti(id_igre, igralec):
     if igra.igraObstaja(id_igre):
         if igra.vrniVojno(id_igre).jeNaVrsti(igralec) or igra.vrniVojno(id_igre).zmagovalec() is not None:
-            return 'true'    
+            return 'true'
         else:
-            return 'false'    
+            return 'false'
     return 'Igra ne obstaja!'
+
 
 @post('/igra/zmagovalec/<id_igre>')
 def zmagovalec(id_igre):
@@ -123,5 +138,6 @@ def zmagovalec(id_igre):
             return str(zmagovalec)
         return 'undefined'
     return 'Igra ne obstaja!'
+
 
 run(host='127.0.0.1', port=5002)
