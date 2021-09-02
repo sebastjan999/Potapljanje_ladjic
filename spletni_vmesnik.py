6c27c9456e1d32f4488cd82bee0c75cbb5cef004
Sebastjan Senk <seba.senk@gmail.com>
2021-09-02 11:29:14 +0200

    skoraj dokoncano

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -57,4 +57,53 @@ def vsiIgralci(id_igre):
 #dokoncaj se danes nujnoooo!!!!!!!!!(drgac bo cajt sou pol bos pa spet joku XD)
 #se za postavt ladjice, pa kko cilat, pa kdo zmagfa/zgubi pa te force treba nardit!!!
 
+@post('/igra/postaviLadjo/<id_igre>')
+def postaviLadjo(id_igre):
+    if igra.igraObstaja(id_igre):
+        igralec = int(request.forms['igralec'])
+        x       = int(request.forms['x'])
+        y       = int(request.forms['y'])
+        n       = int(request.forms['n'])
+        s       = request.forms['s']
+        igra.vrniVojno(id_igre).postaviLadjo(igralec, x, y, n, s)
+        return 'true'
+    return 'Igra ne obstaja!'
+
+@post('/igra/ustreli/<id_igre>')
+def ustreli(id_igre):
+    if igra.igraObstaja(id_igre):
+        igralec = int(request.forms['igralec'])
+        x       = int(request.forms['x'])
+        y       = int(request.forms['y'])
+        return str(igra.vrniVojno(id_igre).ustreli(igralec, x, y))
+    return 'Igra ne obstaja!'
+
+@post('/igra/vseLadje/<id_igre>')
+def vseLadje(id_igre):
+    if igra.igraObstaja(id_igre) and igra.vrniVojno(id_igre).vseLadjePostavljene():
+        return 'true'
+    return 'false'
+
+
+##
+
+@post('/igra/naVrsti/<id_igre>/<igralec:int>')
+def naVrsti(id_igre, igralec):
+    if igra.igraObstaja(id_igre):
+        if igra.vrniVojno(id_igre).jeNaVrsti(igralec) or igra.vrniVojno(id_igre).zmagovalec() is not None:
+            return 'true'    
+        else:
+            return 'false'    
+    return 'Igra ne obstaja!'
+
+@post('/igra/zmagovalec/<id_igre>')
+def zmagovalec(id_igre):
+    if igra.igraObstaja(id_igre):
+        # Ce obstaja zmagovalec vrni njegovo stevilko, drugace 'undefined'
+        zmagovalec = igra.vrniVojno(id_igre).zmagovalec()
+        if zmagovalec is not None:
+            return str(zmagovalec)
+        return 'undefined'
+    return 'Igra ne obstaja!'
+
 ##run(host='127.0.0.1', port=5002)
